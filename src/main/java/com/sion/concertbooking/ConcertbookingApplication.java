38d44c08fcc788288f519c6bf909e12c84553bbc
노시온 <shtldhs123@naver.com>
2025-01-01 02:29:55 +0900

    init commit

---
+++ src/main/java/com/sion/concertbooking/ConcertbookingApplication.java
@@ -0,0 +1,13 @@
+package com.sion.concertbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sion/concertbooking/ConcertbookingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sion.concertbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ConcertbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
