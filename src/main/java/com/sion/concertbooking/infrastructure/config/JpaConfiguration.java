a6a48ed615c06de05607e1d3f7e51e928855e4d9
노시온 <shtldhs123@naver.com>
2025-01-05 17:40:14 +0900

    move: JPA config 패키지 이동

--- src/main/java/com/sion/concertbooking/infrastructure/config/JpaConfiguration.java
+++ src/main/java/com/sion/concertbooking/infrastructure/config/JpaConfiguration.java
@@ -1,4 +1,4 @@
-package com.sion.concertbooking.global.config;
+package com.sion.concertbooking.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
