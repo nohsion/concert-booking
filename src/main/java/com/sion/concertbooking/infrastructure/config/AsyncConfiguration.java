16e0d9d16af41a78f15095d6766394bbef082cd1
노시온 <shtldhs123@naver.com>
2025-01-17 02:24:42 +0900

    fix: 누락된 EnableAsync 추가

--- src/main/java/com/sion/concertbooking/infrastructure/config/AsyncConfiguration.java
+++ src/main/java/com/sion/concertbooking/infrastructure/config/AsyncConfiguration.java
@@ -3,10 +3,12 @@ package com.sion.concertbooking.infrastructure.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+@EnableAsync
 @Configuration
 public class AsyncConfiguration {
 
